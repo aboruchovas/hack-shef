10c1aa93441c0e93c14e36482b7773a699438436
aboruchovas <43833194+aboruchovas@users.noreply.github.com>
2018-10-27 18:59:36 +0100

    Update main.py
    Removed code for smile detection

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import cv2
 import time
 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-smile_cascade = cv2.CascadeClassifier('haarcascade_smile.xml')
 
 video_capture = cv2.VideoCapture(0)
 
@@ -19,22 +17,12 @@ while True:
         minSize=(30, 30)
     )
 
-    smiles = smile_cascade.detectMultiScale(
-        gray,
-        scaleFactor=1.05,
-        minNeighbors=5,
-        minSize=(60, 60)
-    )
-
     # Draw a rectangle around the faces
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
-    for (sx, sy, sw, sh) in smiles:
-        cv2.rectangle(frame, (sx, sy), (sx+sw, sy+sh), (0, 0, 255), 2)
-
     print('Faces found: ', len(faces))
-    print('Smiles found: ', len(smiles))
+    
     # Display the resulting frame
     cv2.imshow('Video', frame)
 
